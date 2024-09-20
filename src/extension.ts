9974d0c076772b839a62b2dd3a1d23f5330a68ab
ipabapi <10969639+ipabapi@users.noreply.github.com>
2024-09-20 13:30:47 -0700

    it works

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+// Spawn a child process
+import { spawn, exec } from "child_process";
+import path from "path";
+
+// Path to the UAsset Editor
+let uassetCLIPath: string = "";
+
+function convert(path: string, type: boolean) {
+  if (!path) {
+    return;
+  }
+  try {
+    exec(
+      `${uassetCLIPath} ${type ? "-j" : "-u"} ${path}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          vscode.window.showErrorMessage(`An error occured: ${error}`);
+          console.error(`exec error: ${error}`);
+        } else {
+          vscode.window.showInformationMessage(`Conversion successful!`);
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "uasset-editor" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('uasset-editor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from UAsset Editor!');
-	});
-
-	context.subscriptions.push(disposable);
+  uassetCLIPath = vscode.Uri.joinPath(
+    context.extensionUri,
+    "assets",
+    "cli",
+    "UAssetCLITool.exe"
+  ).fsPath;
+  let disposable = vscode.commands.registerCommand(
+    "uasset-editor.touasset",
+    (uri: vscode.Uri) => {
+      convert(uri.fsPath, false);
+    }
+  );
+  context.subscriptions.push(disposable);
+  let disposable2 = vscode.commands.registerCommand(
+    "uasset-editor.tojson",
+    (uri: vscode.Uri) => {
+      convert(uri.fsPath, true);
+    }
+  );
+  context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
